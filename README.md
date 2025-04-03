5c54b76ff6864fe9a009db197aaa77c68cc49b35
Ubuntu <ubuntu@ip-172-31-0-193.ap-south-1.compute.internal>
2025-04-03 17:36:02 +0000

    triton: matrix multiplication autotuning

--- README.md
+++ README.md
@@ -8,6 +8,8 @@
 
 |**Day**|**Code**|**Notes**|**Progress**|
 |---|----|-----|--------|
+|064|[triton: matrix multiplication](./triton/matmul.py)|[triton](./notes/005/)|autotuning|
+|063|[triton: fused softmax](./triton/fused_softmax.py)|[triton](./notes/005/)|added benchmarks|
 |062|[triton: fused softmax](./triton/fused_softmax.py)|[triton](./notes/005/)|working fused softmax|
 |061|[triton: fused softmax](./triton/fused_softmax.py)|[triton](./notes/005/)|benchmarking|
 |060|[triton: fused softmax](./triton/fused_softmax.py)|[triton](./notes/005/)|kernel call test function|
---
+++ triton/matmul.py
@@ -0,0 +1,20 @@
+import torch
+import triton 
+import triton.language as tl 
+DEVICE = "cuda"
+
+# in autotuning we set a bunch of hyperparameter configurations that can be used
+# triton will automatically pick the best one for our gpu configuration. 
+# the values are set heuristically, but most of them are multiples of 32 which is the number of threads in a warp. 
+
+autotune_configs = [
+    triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE': 8}, num_stages=3, num_warps=8),
+    triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE': 8}, num_stages=4, num_warps=4),
+    triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE': 8}, num_stages=4, num_warps=4),
+    triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 64, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE': 8}, num_stages=4, num_warps=4),
+    triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE': 8}, num_stages=4, num_warps=4),
+    triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 32, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE': 8}, num_stages=4, num_warps=4),
+    triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 32, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE': 8}, num_stages=5, num_warps=2),
+    triton.Config({'BLOCK_SIZE_M': 32, 'BLOCK_SIZE_N': 64, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE': 8}, num_stages=5, num_warps=2)
+]
+
