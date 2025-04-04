dea1a4e1890907b87d7cedd8a493f3f16f8834be
Ubuntu <ubuntu@ip-172-31-0-193.ap-south-1.compute.internal>
2025-04-04 18:16:28 +0000

    triton: matmul kernel:

--- README.md
+++ README.md
@@ -8,6 +8,7 @@
 
 |**Day**|**Code**|**Notes**|**Progress**|
 |---|----|-----|--------|
+|065|[triton: matrix multiplication](./triton/matmul.py)|[triton](./notes/005/)|matmul kernel|
 |064|[triton: matrix multiplication](./triton/matmul.py)|[triton](./notes/005/)|autotuning|
 |063|[triton: fused softmax](./triton/fused_softmax.py)|[triton](./notes/005/)|added benchmarks|
 |062|[triton: fused softmax](./triton/fused_softmax.py)|[triton](./notes/005/)|working fused softmax|
--- triton/matmul.py
+++ triton/matmul.py
@@ -3,9 +3,9 @@ import triton
 import triton.language as tl 
 DEVICE = "cuda"
 
-# in autotuning we set a bunch of hyperparameter configurations that can be used
-# triton will automatically pick the best one for our gpu configuration. 
-# the values are set heuristically, but most of them are multiples of 32 which is the number of threads in a warp. 
+
+
+
 
 autotune_configs = [
     triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE': 8}, num_stages=3, num_warps=8),
@@ -18,3 +18,37 @@ autotune_configs = [
     triton.Config({'BLOCK_SIZE_M': 32, 'BLOCK_SIZE_N': 64, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE': 8}, num_stages=5, num_warps=2)
 ]
 
+@triton.autotune(configs = autotune_configs, key=['M', 'N', 'K'])
+@triton.jit
+def _matmul_kernel(
+    a_ptr, b_ptr, c_ptr, 
+    M, N, K, 
+    stride_a_M, stride_a_K, 
+    stride_b_K, stride_b_N, 
+    stride_c_M, stride_c_N, 
+    
+    BLOCK_SIZE_M: tl.constexpr, BLOCK_SIZE_N: tl.constexpr, BLOCK_SIZE_K: tl.constexpr,
+    GROUP_SIZE: tl.constexpr, 
+):
+    PID = tl.program_id(axis=0) 
+    
+    num_PID_along_M = tl.cdiv(M, BLOCK_SIZE_M) 
+    num_PID_along_N = tl.cdiv(N, BLOCK_SIZE_N) 
+    num_PID_in_group = GROUP_SIZE * num_PID_along_N
+    
+    group_id = PID // num_PID_in_group 
+    
+    first_PID_in_group_along_M = group_id * GROUP_SIZE 
+    
+    group_size_adj = min(num_PID_along_M - first_PID_in_group_along_M, GROUP_SIZE) 
+    
+    PID_M = first_PID_in_group_along_M + ((PID % num_PID_in_group) % group_size_adj)
+        
+    PID_N = (PID % num_PID_in_group) // group_size_adj
+    
+    offsets_M = PID_M * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+    offsets_N = PID_N * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+    offsets_K = tl.arange(0, BLOCK_SIZE_K)
+    
+    a_offsets = offsets_M[:, None] * stride_a_M + offsets_K[None, :] * stride_a_K
+    b_offsets = offsets_K[:, None] * stride_b_K + offsets_N[None, :] * stride_b_N
