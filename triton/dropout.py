9dde2c021c74014293124f411b625df4c5692f52
Ubuntu <ubuntu@ip-172-31-0-193.ap-south-1.compute.internal>
2025-04-12 16:50:42 +0000

    triton: dropout wrapper

--- triton/dropout.py
+++ triton/dropout.py
@@ -21,3 +21,12 @@ def _seeded_dropout(
     x_keep = random > p 
     output = tl.where(x_keep, x / (1 - p), 0.0)
     tl.store(output_ptr + offsets, output, mask=mask)
+
+
+def seeded_dropout(x, p, seed):
+    output = torch.empty_like(x)
+    assert x.is_contiguous()
+    n_elements = x.numel()
+    grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
+    _seeded_dropout[grid](x, output, n_elements, p, seed, BLOCK_SIZE=1024)
+    return output
